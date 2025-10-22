0c7169e7bf725ac7f44580319216534117dcabaf
Brigham Andersen <brighambandersen@gmail.com>
2025-10-21 21:53:08 -0600

    clean up

--- src/utils.ts
+++ src/utils.ts
@@ -1,31 +1,31 @@
-export const extractUrls = (htmlContent: string): string[] => {
+export function extractUrls(htmlContent: string) {
   const urlRegex = /https?:\/\/[^\s<>"{}|\\^`[\]]+/gi;
   return htmlContent.match(urlRegex) || [];
 };
 
-export const shortenUrl = (url: string): string => {
-  return `https://short.ly/${Math.random().toString(36).substr(2, 8)}`;
+export function generateId() {
+  return `${Math.random().toString(36).substr(2, 8)}`;
 };
 
-// export function shortenUrlsInHtml(htmlContent) {
-//   // Use a regex to find URLs in href/src attributes
-//   const urlRegex = /(?:href|src)=["'](https?:\/\/[^"']+)["']/gi;
-//   const replacements = [];
-//   let match;
+export function shortenUrlsInHtml(htmlContent: string) {
+  // Use a regex to find URLs in href/src attributes
+  const urlRegex = /(?:href|src)=["'](https?:\/\/[^"']+)["']/gi;
+  const replacements = [];
+  let match;
 
-//   // We'll mutate the HTML string as we go
-//   let updatedHtml = htmlContent;
+  // We'll mutate the HTML string as we go
+  let updatedHtml = htmlContent;
 
-//   while ((match = urlRegex.exec(htmlContent)) !== null) {
-//     const originalUrl = match[1];
-//     const id = generateId();
-//     const newUrl = `https://tinyurl.brighamandersen.com/${id}`;
+  while ((match = urlRegex.exec(htmlContent)) !== null) {
+    const originalUrl = match[1] || '';
+    const id = generateId();
+    const newUrl = `https://tinyurl.brighamandersen.com/${id}`;
 
-//     replacements.push({ id, originalUrl, newUrl });
+    replacements.push({ id, originalUrl, newUrl });
 
-//     // Replace only this specific URL instance (not all globally)
-//     updatedHtml = updatedHtml.replace(originalUrl, newUrl);
-//   }
+    // Replace only this specific URL instance (not all globally)
+    updatedHtml = updatedHtml.replace(originalUrl, newUrl);
+  }
 
-//   return { updatedHtml, urls: replacements };
-// }
+  return { updatedHtml, urls: replacements };
+}
