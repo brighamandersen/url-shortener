d46cb0dcfc74dc7dc480bae7b59c92be39f43cab
Brigham Andersen <brighambandersen@gmail.com>
2025-10-22 01:45:31 -0600

    add icon

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.set('views', path.join(__dirname, 'views'));
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.urlencoded({ extended: true })); // Needed for forms
 
 // Initialize database on startup
